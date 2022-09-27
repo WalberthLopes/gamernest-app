80abe13fb297c4d93a130f26c68698004c67697c
Nextti <105963017+WalberthLopes@users.noreply.github.com>
2022-09-27 10:16:18 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./modules/home/Home";
 import Profile from "./modules/account/Profile";
 import Shop from "./modules/shop/Shop";
-import CreateAccount from "./modules/account/CreateAccount";
+//import CreateAccount from "./modules/account/CreateAccount";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/account/create-account" element={<CreateAccount />} />
+        {/* <Route path="/account/create-account" element={<CreateAccount />} /> */}
         <Route path="/shop" element={<Shop />} />
         <Route path="/account/:id" element={<Profile />} />
       </Routes>
