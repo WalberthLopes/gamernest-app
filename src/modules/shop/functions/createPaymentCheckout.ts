ddb330eac753da0922bdff860dc25c9702ed3d2d
Nextti <105963017+WalberthLopes@users.noreply.github.com>
2022-09-29 18:04:52 -0300

    Update createPaymentCheckout.ts

--- src/modules/shop/functions/createPaymentCheckout.ts
+++ src/modules/shop/functions/createPaymentCheckout.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const createPaymentCheckout = async (props: {}, userData: string) => {
-  if (!userData) return;
+const createPaymentCheckout = async (props: {}, user: string) => {
+  if (!user) return;
 
   const response = await axios.post(
     import.meta.env.VITE_API_URL + "/api/create-payment-checkout",
-    { props, userData }
+    { props, user }
   );
 
   return response.data;
